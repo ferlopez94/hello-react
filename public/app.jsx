724328245b511b67fc79fc5282acbd25b39976c3
Fernando Lopez Martinez <fer_fjsb2@hotmail.com>
2016-07-30 20:13:57 -0500

    Add props to Greeter component

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,27 @@
 var Greeter = React.createClass({
+   getDefaultProps: function() {
+      return {
+         name: 'React',
+         message: 'This is from a component!'
+      };
+   },
    render: function() {
+      var name = this.props.name;
+      var message = this.props.message;
+
       return (
          <div>
-            <h1>Hello React!</h1>
-            <p>This is from a component!</p>
+            <h1>Hello {name}!</h1>
+            <p>{message}</p>
          </div>
       );
    }
 });
 
+var firstName = 'Fer';
+var message = 'Hello World!';
+
 ReactDOM.render(
-   <Greeter/>,
+   <Greeter name={firstName} message={message}/>,
    document.getElementById('app')
 );
